a098728c6d8542a3506609e1a7a4c29e8517a981
annashiheart <75702816+annashiheart@users.noreply.github.com>
2022-11-26 00:06:14 -0500

    backtracking solution, bigger canvas

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ Part 5: not started.
 
 Part 6: not started.
 
+## Citations
+
+Board drawing functions adapted from exercises in Animations With 2D Lists (5.3).
+
+Backtracking code (solveSudoku) adapted from solveMiniSudoku (8.11) and isLegalSudoku (4.5).
+
 ## Acknowledgements
 
 Guide: https://www.cs.cmu.edu/~112-3/notes/term-project.html
--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 from cmu_cs3_graphics import *
 import math
 import random
+import copy
 
-def onAppStart(app):
-    app.rows = 9
-    app.cols = 9
-    app.boardLeft = 50
-    app.boardTop = 50
-    app.boardWidth = 300
-    app.boardHeight = 300
-    app.cellBorderWidth = 1
-    app.selection = None
-    app.hoverSelection = None
-    appStarted(app)
+##################################
+# FOR DEBUGGING PURPOSES
+##################################
+def prettyPrint(L):
+    for row in L:
+        print(row)
 
-def appStarted(app):
-    # randomize this later
-    newBoard = readFile('tp-starter-files/boards/easy-01.png.txt')
-    app.board = boardTo2DList(newBoard)
-    app.initialVals = findInitialVals(app.board)
-    app.isGameOver = False
-    app.paused = False
-    app.stepsPerSecond = 1
-    app.score = 0
+##################################
+# CHOOSE AND READ FILE
+##################################
 
 def readFile(path):
     with open(path, "rt") as f:
@@ -39,23 +29,47 @@ def findInitialVals(board):
     initialVals = set()
     for row in range(len(board)):
         for col in range(len(board[0])):
-            num = board[row][col]
-            if (num != '0'):
+            initialNum = board[row][col]
+            if (initialNum != '0'):
                 initialVals.add((row, col))
     return initialVals
 
-def redrawAll(app):
-    drawGameLabels(app)
-    drawBoard(app)
-    drawBoardBorder(app)
-    drawNumberBoxes(app)
-    if app.isGameOver:
-        drawGameOver(app)
+##################################
+# ANIMATION VIEW
+##################################
 
-def drawGameLabels(app):
-    if not app.isGameOver:
-        drawLabel('Sudoku', 200, 20, size=16)
-        drawLabel('Term project', 200, 36, size = 12)
+def onAppStart(app):
+    app.width = 1200
+    app.height = 800
+    app.rows = 9
+    app.cols = 9
+    app.boardLeft = 300
+    app.boardTop = 50
+    app.boardWidth = 600
+    app.boardHeight = 600
+    app.cellBorderWidth = 2
+    app.boardBorderWidth = 2 * app.cellBorderWidth
+    app.selection = None
+    app.hoverSelection = None
+    appStarted(app)
+
+def appStarted(app):
+    # randomize this later
+    newBoard = readFile('tp-starter-files/boards/easy-01.png.txt')
+    app.board = boardTo2DList(newBoard)
+    app.solutionBoard = solveSudoku(app, app.board)
+    app.initialVals = findInitialVals(app.board)
+    app.isGameOver = False
+    app.paused = False
+    app.stepsPerSecond = 1
+    app.score = 0
+
+def redrawAll(app):
+    drawBoard(app)
+    drawNumberBoxes(app)
+    drawBoardBorder(app)
+    if app.isGameOver:
+        drawGameOver(app) # change this to a screen!
 
 def drawGameOver(app):
     drawRect(app.boardLeft, app.boardTop, app.boardWidth, 
@@ -70,13 +84,74 @@ def drawNumberBoxes(app):
     cellWidth, cellHeight = getCellSize(app)
     for i in range(9):
         drawRect(app.boardLeft + i*(app.boardWidth/9), 
-                app.boardTop + app.boardHeight + 5, 
+                app.boardTop + app.boardHeight + 20, 
                 app.boardWidth/9, app.boardHeight/9, 
                 fill = 'lavenderBlush', border = 'black', borderWidth = 1)
         drawLabel(i+1, app.boardLeft + i*(app.boardWidth/9) + cellWidth/2, 
-                app.boardTop + app.boardHeight + 5 + cellHeight/2, size = 16)
-    drawRect(app.boardLeft, app.boardTop + app.boardHeight + 5,
-            app.boardWidth, app.boardHeight/9, fill = None, border = 'black')
+                app.boardTop + app.boardHeight + 20 + cellHeight/2, size = 28)
+    # draw border for number boxes
+    drawRect(app.boardLeft, app.boardTop + app.boardHeight + 20,
+            app.boardWidth, app.boardHeight/9, fill = None, border = 'black',
+            borderWidth = app.cellBorderWidth)
+    # draw delete symbol
+    trashSymbol = chr(0x1f5d1)
+    drawRect(app.boardLeft + 9.5 * (app.boardWidth/9), app.boardTop + app.boardHeight + 20, 
+            app.boardWidth/9, app.boardHeight/9, 
+            fill = 'gainsboro', border = 'black', borderWidth = app.cellBorderWidth)
+    drawLabel(trashSymbol, app.boardLeft + 9.5*(app.boardWidth/9) + cellWidth/2, 
+            app.boardTop + app.boardHeight + 20 + cellHeight/2, size = 28, font='symbols')
+
+                
+def drawBoard(app):
+    for row in range(app.rows):
+        for col in range(app.cols):
+            drawCell(app, row, col)
+            cellNum = app.board[row][col]
+            if (cellNum != '0'):
+                drawCellNum(app, row, col, cellNum)
+
+def drawBoardBorder(app):
+    # draw the blocks
+    for i in range(3):
+        for j in range(3):
+            drawRect(app.boardLeft + i*(app.boardWidth/3), 
+                    app.boardTop + j*(app.boardHeight/3), 
+                    app.boardWidth/3, app.boardHeight/3,
+                    fill=None, border='black',
+                    borderWidth = app.boardBorderWidth)
+    # draw the outer border
+    drawRect(app.boardLeft, app.boardTop, app.boardWidth, app.boardHeight,
+            fill=None, border='black', borderWidth = 2*app.boardBorderWidth)
+
+def drawCell(app, row, col):
+    cellLeft, cellTop = getCellLeftTop(app, row, col)
+    cellWidth, cellHeight = getCellSize(app)
+    # differentiate initial and player numbers
+    if (row, col) in app.initialVals:
+        color = 'gainsboro'
+    elif (row, col) == app.selection:
+        color = 'lightBlue'
+    elif (row, col) == app.hoverSelection:
+        color = 'aliceBlue'
+    else:
+        color = None
+    drawRect(cellLeft, cellTop, cellWidth, cellHeight,
+           fill=color, border='black', borderWidth=app.cellBorderWidth)
+
+def drawCellNum(app, row, col, cellNum):
+    cellLeft, cellTop = getCellLeftTop(app, row, col)
+    cellWidth, cellHeight = getCellSize(app)
+    # change number color if right or wrong
+    if cellNum == app.solutionBoard[row][col]:
+        color = 'black'
+    else:
+        color = 'red'
+    drawLabel(cellNum, cellLeft + cellWidth/2, cellTop + cellHeight/2, 
+            fill=color, size = 28,)
+
+##################################
+# ANIMATION KEY AND MOUSE
+##################################
 
 def onMousePress(app, mouseX, mouseY):
     if not app.isGameOver:
@@ -87,16 +162,24 @@ def onMousePress(app, mouseX, mouseY):
                 app.selection = selectedCell 
             else:
                 app.selection = None
+        # fill cell by mouse
         elif (selectedNum != None) and (app.selection != None):
             row, col = app.selection
             app.board[row][col] = str(selectedNum)
+        # delete number by mouse
+        elif ((app.selection != None) and
+            (app.boardLeft + 9.5 * (app.boardWidth/9) <= mouseX <= app.boardLeft + 10.5 * (app.boardWidth/9)) and 
+            (app.boardTop + app.boardHeight + 20 <= mouseY <= app.boardTop + app.boardHeight + 20 + app.boardHeight/9)):
+            row, col = app.selection
+            app.board[row][col] = '0'
 
 def onMouseMove(app, mouseX, mouseY):
     if not app.isGameOver:
         selectedCell = getCell(app, mouseX, mouseY)
+        selectedNum = getNumberFromBox(app, mouseX, mouseY)
         if (selectedCell != None) and (selectedCell != app.selection):
             app.hoverSelection = selectedCell
-        else:
+        else: # add hover over number boxes
             app.hoverSelection = None
 
 def onKeyPress(app, key):
@@ -110,49 +193,10 @@ def onKeyPress(app, key):
     if (app.selection != None) and (key == 'backspace'):
         row, col = app.selection
         app.board[row][col] = '0'
-                
-def drawBoard(app):
-    for row in range(app.rows):
-        for col in range(app.cols):
-            drawCell(app, row, col)
-            num = app.board[row][col]
-            if (num != '0'):
-                drawNum(app, row, col, num)
 
-def drawBoardBorder(app):
-    # draw the board outline (with double-thickness):
-    for i in range(3):
-        for j in range(3):
-            drawRect(app.boardLeft + i*(app.boardWidth/3), 
-                    app.boardTop + j*(app.boardHeight/3), 
-                    app.boardWidth/3, app.boardHeight/3,
-                    fill=None, border='black',
-                    borderWidth=2*app.cellBorderWidth)
-    drawRect(app.boardLeft, app.boardTop, app.boardWidth, app.boardHeight,
-            fill=None, border='black', borderWidth=4*app.cellBorderWidth)
-
-def drawCell(app, row, col):
-    cellLeft, cellTop = getCellLeftTop(app, row, col)
-    cellWidth, cellHeight = getCellSize(app)
-    # add diff color for initial vals and added vals
-    if (row, col) in app.initialVals:
-        color = 'gainsboro'
-    elif (row, col) == app.selection:
-        color = 'pink'
-    elif (row, col) == app.hoverSelection:
-        color = 'lavenderBlush'
-    else:
-        color = None
-    drawRect(cellLeft, cellTop, cellWidth, cellHeight,
-           fill=color, border='black',
-           borderWidth=app.cellBorderWidth)
-
-def drawNum(app, row, col, num):
-    cellLeft, cellTop = getCellLeftTop(app, row, col)
-    cellWidth, cellHeight = getCellSize(app)
-    color = 'black'
-    drawLabel(num, cellLeft + cellWidth/2, cellTop + cellHeight/2, 
-            fill=color, size = 16)
+##################################
+# ANIMATION CONTROLLER
+##################################
 
 def getCell(app, x, y):
     dx = x - app.boardLeft
@@ -182,6 +226,78 @@ def getNumberFromBox(app, x, y):
             (app.boardTop + app.boardHeight + 5 <= y <= app.boardTop + app.boardHeight + 5 + app.boardHeight/9)):
             return i+1
     return None
+
+
+##################################
+# FIND BOARD SOLUTION
+##################################
+
+def solveSudoku(app, board):
+    board = copy.deepcopy(board) # is this bad
+    if findNextEmptyCell(app, board) == None:
+        return board
+    else:
+        row, col = findNextEmptyCell(app, board)
+        for i in range(1,10):
+            board[row][col] = str(i)
+            if isBoardLegal(app, board):
+                solution = solveSudoku(app, board) 
+                if solution != None:
+                    return solution
+            board[row][col] = '0'
+        return None
+
+def findNextEmptyCell(app, board):
+    for row in range(app.rows):
+        for col in range(app.cols):
+            if board[row][col] == '0':
+                return row, col
+    return None
+
+def isBoardLegal(app, board):
+    for row in range(app.rows):
+        if not isLegalRow(board, row):
+            return False
+    for col in range(app.cols):
+        if not isLegalCol(board, col):
+            return False
+    blocks = app.rows
+    for block in range(blocks):
+        if not isLegalBlock(board, block):
+            return False
+    return True
+
+def areLegalValues(L):
+    seen = set()
+    for value in L:
+        # value in not duplicate
+        if value != '0' and value in seen:
+            return False
+        seen.add(value)
+    return True
+
+def isLegalRow(board, row):
+    return areLegalValues(board[row])
+    
+def isLegalCol(board, col):
+    rows = len(board)
+    values = [board[row][col] for row in range(rows)]
+    return areLegalValues(values)
+
+def isLegalBlock(board, block):
+    blockSize = 3
+    startRow = block // blockSize * blockSize
+    startCol = block % blockSize * blockSize
+    values = []
+    for drow in range(blockSize):
+        for dcol in range(blockSize):
+            row, col = startRow + drow, startCol + dcol
+            values.append(board[row][col])
+    return areLegalValues(values)
+
+##################################
+# RUN APP
+##################################
 
 def main():
     runApp()
